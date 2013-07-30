b5b59ffe9ce5e32353879ac917cbaa9447ba37ef
MrBagamen <mrbagamen@gmail.com>
2013-07-30 15:45:23 +0200

    gosu pushing blocks but cant shot collision

--- src/player.cpp
+++ src/player.cpp
@@ -19,10 +19,14 @@ void Player::OnKeyPressed(sf::Keyboard::Key key)
         for(unsigned int i = 0; i < map.movable.size(); i++)
         {
             Movable& m = map.movable[i];
-            printf("X: %d, Y: %d\n", m.x, m.y);
             if(m.x == x && m.y == y)
             {
-                m.x -= speed;
+                m.x -= 32;
+            }
+            if(map.isWallAt(m.x, m.y))
+            {
+                m.x += 32;
+                x += speed;
             }
         }
 
@@ -34,6 +38,20 @@ void Player::OnKeyPressed(sf::Keyboard::Key key)
             x += speed;
             direction = RIGHT;
         }
+        for(unsigned int i = 0; i < map.movable.size(); i++)
+        {
+            Movable& m = map.movable[i];
+            printf("X: %d, Y: %d\n", m.x, m.y);
+            if(m.x == x && m.y == y)
+            {
+                m.x += 32;
+            }
+            if(map.isWallAt(m.x, m.y))
+            {
+                m.x -= 32;
+                x -= speed;
+            }
+        }
 
         break;
 
@@ -43,6 +61,20 @@ void Player::OnKeyPressed(sf::Keyboard::Key key)
             y -= speed;
             direction = UP;
         }
+        for(unsigned int i = 0; i < map.movable.size(); i++)
+        {
+            Movable& m = map.movable[i];
+            printf("X: %d, Y: %d\n", m.x, m.y);
+            if(m.x == x && m.y == y)
+            {
+                m.y -= 32;
+            }
+            if(map.isWallAt(m.x, m.y))
+            {
+                m.y += 32;
+                y += speed;
+            }
+        }
 
         break;
 
@@ -51,6 +83,20 @@ void Player::OnKeyPressed(sf::Keyboard::Key key)
         {
             y += speed;
             direction = DOWN;
+        }
+        for(unsigned int i = 0; i < map.movable.size(); i++)
+        {
+            Movable& m = map.movable[i];
+            printf("X: %d, Y: %d\n", m.x, m.y);
+            if(m.x == x && m.y == y)
+            {
+                m.y += 32;
+            }
+            if(map.isWallAt(m.x, m.y))
+            {
+                m.y -= 32;
+                y -= speed;
+            }
         }
 
         break;
